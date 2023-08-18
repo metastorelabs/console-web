757c2f1ca6c62c0de836532ddaa01a3076b98414
santhosh <august.vs.sant@gmail.com>
2023-08-18 11:34:19 +0530

    feat(DEV-19): add landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='bg-gray-950'>{children}</body>
     </html>
   )
 }
