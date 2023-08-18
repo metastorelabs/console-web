f8b3e4616524fcd0b591982050e159029f801436
santhosh <august.vs.sant@gmail.com>
2023-08-18 11:59:20 +0530

    feat(DEV-19): add fadein effect for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { HiCloudArrowUp, HiCog6Tooth, HiFingerPrint, HiOutlineShoppingBag, HiSer
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+
 const primaryFeatures = [
   {
     name: 'Metastore Tools',
@@ -139,30 +141,154 @@ const Landing = () => {
 
           <div className='absolute inset-0 bg-black -z-10 opacity-80' />
 
-          <div className='mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40 lg:min-h-screen'>
-            <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8'>
-              <Image className='h-12 w-12' src='/logo.png' alt='Metastore logo' width={48} height={48} />
+          <FadeIn>
+            <div className='mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40 lg:min-h-screen'>
+              <div className='mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8'>
+                <Image className='h-12 w-12' src='/logo.png' alt='Metastore logo' width={48} height={48} />
 
-              <div className='mt-24 sm:mt-32 lg:mt-16 flex'>
-                <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20'>
-                  Announcing our new onramp API.
-                  <a href='#' className='font-semibold text-white ml-2'>
-                    <span className='absolute inset-0' aria-hidden='true' />
-                    Read more <span aria-hidden='true'>&rarr;</span>
+                <div className='mt-24 sm:mt-32 lg:mt-16 flex'>
+                  <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20'>
+                    Announcing our new onramp API.
+                    <a href='#' className='font-semibold text-white ml-2'>
+                      <span className='absolute inset-0' aria-hidden='true' />
+                      Read more <span aria-hidden='true'>&rarr;</span>
+                    </a>
+                  </div>
+                </div>
+
+                <h1 className='mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl'>
+                  Start distributing your games in Metastore.
+                </h1>
+                <p className='mt-6 text-lg leading-8 text-gray-300'>
+                  Give your web2 and web3 games the audience it deserves.
+                </p>
+                <div className='mt-10 flex items-center gap-x-6'>
+                  <a
+                    href='#'
+                    className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400'
+                  >
+                    Enter console
                   </a>
+                  <Link
+                    href='https://docs.metastore.to'
+                    className='text-sm font-semibold leading-6 text-white'
+                    target='_blank'
+                  >
+                    Go to Docs <span aria-hidden='true'>→</span>
+                  </Link>
                 </div>
               </div>
+            </div>
+          </FadeIn>
+        </div>
 
-              <h1 className='mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl'>
-                Start distributing your games in Metastore.
-              </h1>
+        {/* Feature section */}
+        <FadeIn>
+          <div className='mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8'>
+            <div className='mx-auto max-w-2xl text-center'>
+              <h2 className='text-base font-semibold leading-7 text-indigo-400'>Distribute faster</h2>
+              <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+                Everything you need to build and distribute your games.
+              </p>
               <p className='mt-6 text-lg leading-8 text-gray-300'>
+                Metastore is a platform for distributing your games to a wide audience. We provide you with the tools to
+                build, distribute and monetize your games.
+              </p>
+            </div>
+            <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none'>
+              <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3'>
+                {primaryFeatures.map((feature) => (
+                  <div key={feature.name} className='flex flex-col'>
+                    <dt className='text-base font-semibold leading-7 text-white'>
+                      <div className='mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500'>
+                        <feature.icon className='h-6 w-6 text-white' aria-hidden='true' />
+                      </div>
+                      {feature.name}
+                    </dt>
+                    <dd className='mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300'>
+                      <p className='flex-auto'>{feature.description}</p>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </FadeIn>
+
+        {/* Feature section */}
+        <div className='mt-32 sm:mt-56'>
+          <FadeIn>
+            <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+              <div className='mx-auto max-w-2xl sm:text-center'>
+                <h2 className='text-base font-semibold leading-7 text-indigo-400'>Metastore Console</h2>
+                <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+                  Self-service publishing tools.
+                </p>
+                <p className='mt-6 text-lg leading-8 text-gray-300'>
+                  Distribute your Games and NFTs to the Metastore audience with our self-service publishing tools.
+                </p>
+              </div>
+            </div>
+            <div className='relative overflow-hidden pt-16'>
+              <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+                <Image
+                  src='/console-ss.png'
+                  alt='App screenshot'
+                  className='mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10'
+                  width={2432}
+                  height={1442}
+                />
+                <div className='relative' aria-hidden='true'>
+                  <div className='absolute -inset-x-20 bottom-0 bg-gradient-to-t from-gray-950 pt-[7%]' />
+                </div>
+              </div>
+            </div>
+          </FadeIn>
+          <FadeInStagger>
+            <div className='mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8'>
+              <dl className='mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16'>
+                {secondaryFeatures.map((feature) => (
+                  <FadeIn key={feature.name}>
+                    <div className='relative pl-9'>
+                      <dt className='inline font-semibold text-white'>
+                        <feature.icon className='absolute left-1 top-1 h-5 w-5 text-indigo-500' aria-hidden='true' />
+                        {feature.name}
+                      </dt>{' '}
+                      <dd className='inline'>{feature.description}</dd>
+                    </div>
+                  </FadeIn>
+                ))}
+              </dl>
+            </div>
+          </FadeInStagger>
+        </div>
+
+        {/* CTA section */}
+        <FadeIn>
+          <div className='relative isolate mt-32 px-6 py-32 sm:mt-56 sm:py-40 lg:px-8'>
+            <div
+              className='absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl'
+              aria-hidden='true'
+            >
+              <div
+                className='aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20'
+                style={{
+                  clipPath:
+                    'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
+                }}
+              />
+            </div>
+            <div className='mx-auto max-w-2xl text-center'>
+              <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
+                Start distributing your games in Metastore.
+              </h2>
+              <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300'>
                 Give your web2 and web3 games the audience it deserves.
               </p>
-              <div className='mt-10 flex items-center gap-x-6'>
+              <div className='mt-10 flex items-center justify-center gap-x-6'>
                 <a
                   href='#'
-                  className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400'
+                  className='rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
                 >
                   Enter console
                 </a>
@@ -176,119 +302,7 @@ const Landing = () => {
               </div>
             </div>
           </div>
-        </div>
-
-        {/* Feature section */}
-        <div className='mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8'>
-          <div className='mx-auto max-w-2xl text-center'>
-            <h2 className='text-base font-semibold leading-7 text-indigo-400'>Distribute faster</h2>
-            <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-              Everything you need to build and distribute your games.
-            </p>
-            <p className='mt-6 text-lg leading-8 text-gray-300'>
-              Metastore is a platform for distributing your games to a wide audience. We provide you with the tools to
-              build, distribute and monetize your games.
-            </p>
-          </div>
-          <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none'>
-            <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3'>
-              {primaryFeatures.map((feature) => (
-                <div key={feature.name} className='flex flex-col'>
-                  <dt className='text-base font-semibold leading-7 text-white'>
-                    <div className='mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500'>
-                      <feature.icon className='h-6 w-6 text-white' aria-hidden='true' />
-                    </div>
-                    {feature.name}
-                  </dt>
-                  <dd className='mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300'>
-                    <p className='flex-auto'>{feature.description}</p>
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </div>
-
-        {/* Feature section */}
-        <div className='mt-32 sm:mt-56'>
-          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-            <div className='mx-auto max-w-2xl sm:text-center'>
-              <h2 className='text-base font-semibold leading-7 text-indigo-400'>Metastore Console</h2>
-              <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-                Self-service publishing tools.
-              </p>
-              <p className='mt-6 text-lg leading-8 text-gray-300'>
-                Distribute your Games and NFTs to the Metastore audience with our self-service publishing tools.
-              </p>
-            </div>
-          </div>
-          <div className='relative overflow-hidden pt-16'>
-            <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-              <Image
-                src='/console-ss.png'
-                alt='App screenshot'
-                className='mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10'
-                width={2432}
-                height={1442}
-              />
-              <div className='relative' aria-hidden='true'>
-                <div className='absolute -inset-x-20 bottom-0 bg-gradient-to-t from-gray-950 pt-[7%]' />
-              </div>
-            </div>
-          </div>
-          <div className='mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8'>
-            <dl className='mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16'>
-              {secondaryFeatures.map((feature) => (
-                <div key={feature.name} className='relative pl-9'>
-                  <dt className='inline font-semibold text-white'>
-                    <feature.icon className='absolute left-1 top-1 h-5 w-5 text-indigo-500' aria-hidden='true' />
-                    {feature.name}
-                  </dt>{' '}
-                  <dd className='inline'>{feature.description}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </div>
-
-        {/* CTA section */}
-        <div className='relative isolate mt-32 px-6 py-32 sm:mt-56 sm:py-40 lg:px-8'>
-          <div
-            className='absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl'
-            aria-hidden='true'
-          >
-            <div
-              className='aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20'
-              style={{
-                clipPath:
-                  'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
-              }}
-            />
-          </div>
-          <div className='mx-auto max-w-2xl text-center'>
-            <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-              Start distributing your games in Metastore.
-            </h2>
-            <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300'>
-              Give your web2 and web3 games the audience it deserves.
-            </p>
-            <div className='mt-10 flex items-center justify-center gap-x-6'>
-              <a
-                href='#'
-                className='rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
-              >
-                Enter console
-              </a>
-              <Link
-                href='https://docs.metastore.to'
-                className='text-sm font-semibold leading-6 text-white'
-                target='_blank'
-              >
-                Go to Docs <span aria-hidden='true'>→</span>
-              </Link>
-            </div>
-          </div>
-        </div>
+        </FadeIn>
       </main>
 
       {/* Footer */}
