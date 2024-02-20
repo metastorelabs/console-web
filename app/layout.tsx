cdb11366eb0083c9ddeac3e092febfa02fcd10a1
santhosh <august.vs.sant@gmail.com>
2024-02-20 11:37:27 +0530

    chore(DEV-19): add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,36 @@ import localFont from 'next/font/local'
 
 import NotificationToaster from '@/components/NotificationToaster'
 
+import type { Metadata } from 'next'
+
 const ailerons = localFont({
   variable: '--font-ailerons',
   src: '../assets/fonts/Ailerons.otf',
   display: 'swap',
 })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Console - Metastore',
+  description: 'Post and manage you game in Metastore with our tools and services.',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        rel: 'icon',
+        sizes: '48x48',
+      },
+      {
+        url: '/favicon.svg',
+        rel: 'icon',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        url: '/apple-touch-icon.png',
+        rel: 'apple-touch-icon',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
